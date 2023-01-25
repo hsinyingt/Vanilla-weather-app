25853e4c3c9d3c7768ca5c8f7aaebfa3e3dac7ca
hanatsai <hsinyingtsaiatelier@gmail.com>
2023-01-25 07:11:14 +0100

    api connected

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ function formatData(timestamp) {
   return `${day} ${hours}:${minute}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thu", "Fri", "Sat", "Sun"];
@@ -53,6 +53,13 @@ function displayForecast() {
   console.log(forecastHTML);
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "2d96d64425dca1d6eda00d942a281c0d";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayWeatherCondition(response) {
   document.querySelector("#searchCity").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(
@@ -78,6 +85,7 @@ function displayWeatherCondition(response) {
     .setAttribute("alt", response.data.weather[0].description);
 
   celsiusTemperature = response.data.main.temp;
+  getForecast(response.data.coord);
 }
 
 function searchCity(city) {
@@ -138,4 +146,3 @@ let fahrenheitLink = document.querySelector("#metricFahrenheit");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 searchCity("Prague");
-displayForecast();
